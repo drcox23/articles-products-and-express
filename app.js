298bc380daa3588bcde4b9c4a1f5ac45524426e9
doug <drcox23@gmail.com>
2018-08-30 19:34:20 -1000

    update the package.json with body-parser and express-handlebars

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const app = express();
 const bp = require('body-parser');
 const exphbs = require('express-handlebars');
 
+app.use(express.static('public'));
+
+app.use(bp.urlencoded({ extended: true }));
+
+app.engine('.hbs', exphbs({ layout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
 
 app.get('/', (req, res) => {
   res.send('Aloha');
+})
+
+//render items
+app.get('/', (req, res) => {
+  
 })
 
 app.listen(process.env.PORT, () => {
